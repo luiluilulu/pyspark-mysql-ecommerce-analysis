650a93dc24437c83489481df21257cf8cd366a7a
LuLuluilui <ludeyong12345@gmail.com>
2026-05-17 14:44:56 +0800

    可视化

---
+++ viz/visualize_analysis.py
@@ -0,0 +1,130 @@
+from utils.mysql_utils import get_mysql_connection
+from utils.path_utils import VIZ_OUTPUT_DIR
+
+import pandas as pd
+import plotly.express as px
+
+
+def query_to_df(sql):
+    #Execute SQL and return a DataFrame
+    conn = get_mysql_connection()
+    df = pd.read_sql(sql,conn)
+    conn.close()
+    return df
+
+def save_fig(fig,filename):
+    #Save fig
+    VIZ_OUTPUT_DIR.mkdir(parents=True,exist_ok=True)
+    
+    output_path = VIZ_OUTPUT_DIR / filename
+    fig.write_html(str(output_path))
+
+    print("saved:",output_path)
+
+def plot_behavior_type_count():
+    sql = """
+        SELECT behavior_type , COUNT(*) AS cnt
+        FROM user_behavior
+        GROUP BY behavior_type
+        ORDER BY COUNT(*) DESC;"""
+
+    df = query_to_df(sql)
+
+    fig = px.bar(
+        data_frame= df,
+        x="behavior_type",
+        y="cnt",
+        title="行为类型分布",
+        color="behavior_type",
+        text = "cnt",
+        labels={
+            "behavior_type":"行为类型",
+            "cnt" : "数量"
+        },
+        template= "plotly_white"
+        )
+
+    save_fig(fig,"behavior_type.html")
+    
+def plot_daily_pv_uv():
+    sql = """
+        SELECT behavior_date,COUNT(*) AS pv_cnt,COUNT(DISTINCT user_id) AS user_cnt
+        FROM user_behavior
+        WHERE behavior_type='pv'
+        GROUP BY behavior_date
+        ORDER BY behavior_date;"""
+    df = query_to_df(sql)
+
+    fig = px.line(
+        data_frame=df,
+        x="behavior_date",
+        y=["pv_cnt","user_cnt"],
+        title= "每日浏览量/浏览用户数",
+        labels={
+            "behavior_date": "日期",
+            "pv_cnt" :"浏览量",
+            "user_cnt":"用户量"
+        },
+        markers= True
+        )
+    save_fig(fig,"daily_pv_uv.html")
+
+def plot_hourly_behavior_count():
+    sql = """
+        SELECT behavior_hour, COUNT(*) AS behavior_cnt
+        FROM user_behavior
+        GROUP BY behavior_hour
+        ORDER BY behavior_hour;"""
+    df = query_to_df(sql)
+    fig = px.bar(
+        data_frame=df,
+        x="behavior_hour",
+        y="behavior_cnt",
+        labels={
+            "behavior_hour":"小时",
+            "behavior_cnt":"总行为量"
+        },
+        title="历史每小时行为量",
+        template="plotly_white"
+    )
+    fig.update_xaxes(
+        tickmode = "linear",
+        dtick = 2,
+        range=[-0.5,23.5]
+    )
+    fig.update_yaxes(tickformat=".2s")
+    save_fig(fig,"hourly_behavior_count.html")
+    
+def plot_top10_items():
+    sql = """
+        SELECT item_id , COUNT(*) AS all_behavior_cnt
+        FROM user_behavior
+        GROUP BY item_id
+        ORDER BY all_behavior_cnt DESC
+        LIMIT 10; """
+    df = query_to_df(sql)
+    df["item_id"] = df["item_id"].astype(str)
+    fig = px.bar(
+            data_frame= df,
+            x= "all_behavior_cnt",
+            y="item_id",
+            title="热门商品TOP10",
+            labels={
+                "all_behavior_cnt":"总行为量",
+                "item_id":"商品id"
+            },
+            orientation="h",
+            height=500
+            )
+    fig.update_yaxes(autorange = "reversed")
+    save_fig(fig,"top10_items.html")
+
+
+def main():
+    plot_behavior_type_count()
+    plot_daily_pv_uv()
+    plot_hourly_behavior_count()
+    plot_top10_items()
+
+if __name__ == "__main__":
+    main()
